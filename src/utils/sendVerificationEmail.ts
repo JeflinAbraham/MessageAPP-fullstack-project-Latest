bdd2ca9adfe05c37c0eadee4bb469fb30cbaad44
JeflinAbraham <jeflinabraham21@gmail.com>
2024-06-10 11:55:47 +0530

    resend email configuration

---
+++ src/utils/sendVerificationEmail.ts
@@ -0,0 +1,24 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/verificationEmail";
+import { ApiResponse } from '@/types/ApiResponse';
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+
+  // the return type of this function is a promise of type ApiResponse
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: email,
+      subject: 'Mystery Message Verification Code',
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: true, message: 'Verification email sent successfully.' };
+  } 
+  catch (emailError) {
+    return { success: false, message: 'Failed to send verification email.' };
+  }
+}
