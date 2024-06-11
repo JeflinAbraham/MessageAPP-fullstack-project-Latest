4252100259a8295bbe878c255187dc9828b6bee7
JeflinAbraham <jeflinabraham21@gmail.com>
2024-06-11 15:07:16 +0530

    added home dashboard buttons in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,21 @@
 import React from 'react';
 import Link from 'next/link';
 import { useSession, signOut } from 'next-auth/react';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 import { Button } from './ui/button';
 import { User } from 'next-auth';
+import { X } from 'lucide-react';
+import { useRouter, usePathname } from 'next/navigation';
 
 function Navbar() {
 
@@ -20,6 +33,10 @@ function Navbar() {
     const { data: session } = useSession();
     const user: User = session?.user;
 
+    const router = useRouter();
+    const currPath = usePathname();
+    const isHomePage = currPath === '/';
+
     return (
         <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
             <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
@@ -31,10 +48,44 @@ function Navbar() {
                         <span className="mr-4">
                             Welcome, {user.username || user.email}
                         </span>
-                        {/* nextAuth automatically handles redirection after excecuting signOut() */}
-                        <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>
-                            Logout
+
+                        {isHomePage? (
+                            <Button className="bg-white text-black hover:bg-slate-200 mr-4"
+                                onClick={() => router.replace('/dashboard')}
+                            >
+                                <span>View dashboard</span>
+                            </Button>
+
+                        ): (
+                            <Button className="bg-white text-black hover:bg-slate-200 mr-4"
+                            onClick={() => router.replace('/')}
+                        >
+                            <span>Home</span>
                         </Button>
+                        )}
+
+                        {/* nextAuth automatically handles redirection after excecuting signOut() */}
+
+                        <AlertDialog>
+                            <AlertDialogTrigger asChild>
+                                <Button className="bg-white text-black hover:bg-slate-200">
+                                    <span>Logout</span>
+                                </Button>
+                            </AlertDialogTrigger>
+                            <AlertDialogContent>
+                                <AlertDialogHeader>
+                                    <AlertDialogTitle>Are you sure you want to logout? Click on 'No' to cancel</AlertDialogTitle>
+                                </AlertDialogHeader>
+                                <AlertDialogFooter>
+                                    <AlertDialogCancel>No</AlertDialogCancel>
+                                    <AlertDialogAction onClick={() => signOut()}>Yes</AlertDialogAction>
+                                </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
+
+
+
+
                     </div>
                 ) : (
                     <Link href="/sign-in">
